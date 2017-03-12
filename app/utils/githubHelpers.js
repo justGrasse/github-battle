5f9a6a9c5c390390512cbabc29dc31216d8ce035
Justin Grasse <just.grasse@gmail.com>
2017-03-12 14:08:15 -0700

    create GithubHelpers to create a battle

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -8,6 +8,36 @@ function getUserInfo (username) {
   return axios.get('https://api.github.com/users/' + username + param);
 }
 
+function getRepos (username) {
+  return axios.get('https://api.github.com/users/'
+                      + username + '/repos'
+                      + param + '&per_page=100');
+}
+
+function getTotalStars (repos) {
+  return repos.data.reduce(function (prev, current) {
+    return prev + current.stargazers_count
+  }, 0)
+}
+
+function getPlayersData (player) {
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function (totalStars) {
+      return {
+        followers: players.followers,
+        totalStars: totalStars
+      }
+    })
+}
+
+function calculateScores (players) {
+  return  [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars
+  ]
+}
+
 var helpers = {
   getPlayersInfo: function (players) {
     return axios.all(players.map(function (username) {
@@ -18,7 +48,19 @@ var helpers = {
         return user.data
       })
     })
-    .catch(function (err) {console.warn('Error in getPlayersInfo: ', err)})
+    .catch(function (err) {
+      console.warn('Error in getPlayersInfo: ', err)
+    })
+  },
+  battle: function (players) {
+    var playerOneData =getPlayersData(players[0]);
+    var playerTwoData =getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function (err) {
+        console.warn('Error in battle', err)
+      })
   }
 };
 
